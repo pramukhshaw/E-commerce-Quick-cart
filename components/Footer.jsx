9795cc0e90db8ef2c3b3fde64a5f3053abfc4618
pramukh shaw <shawpramukhjk355@gmail.com>
2025-06-28 12:27:19 +0530

    First Commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,7 @@ const Footer = () => {
         <div className="w-4/5">
           <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
           <p className="mt-6 text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+           Quick Cart – Your one-stop online shop for fast, reliable, and secure shopping. Discover a wide range of products at unbeatable prices, delivered right to your doorstep. Easy checkout, real-time tracking, and 24/7 customer support – everything you need for a hassle-free shopping experience.
           </p>
         </div>
 
@@ -40,14 +37,14 @@ const Footer = () => {
           <div>
             <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
             <div className="text-sm space-y-2">
-              <p>+1-234-567-890</p>
-              <p>contact@greatstack.dev</p>
+              <p>+91-9874197192</p>
+              <p>shawpramukhjk355@gmail.com</p>
             </div>
           </div>
         </div>
       </div>
       <p className="py-4 text-center text-xs md:text-sm">
-        Copyright 2025 © GreatStack.dev All Right Reserved.
+        Copyright 2025 © Pramukh Shaw All Right Reserved.
       </p>
     </footer>
   );

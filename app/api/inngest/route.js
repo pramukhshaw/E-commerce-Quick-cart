a2803e8feabda9cdf5f60a42bded6b68659f084c
pramukh shaw <shawpramukhjk355@gmail.com>
2025-06-28 13:48:11 +0530

    done

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,12 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { inngest } from "../../../inngest/client";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
-export const { GET, POST, PUT } = serve(
-  inngest,
-  [
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
     syncUserCreation,
     syncUserUpdation,
     syncUserDeletion
-  ]
-);
+  ],
+});

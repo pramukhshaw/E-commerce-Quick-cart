e39ce8b0ad3293e2d1e4f5b053835f29b005813f
pramukh shaw <shawpramukhjk355@gmail.com>
2025-06-29 15:32:20 +0530

    done route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,14 @@
-import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+// app/api/inngest/route.js
 
+import { serve } from "inngest/next";
+import { inngest } from "@/inngest/client"; // Use alias if set up properly
+import {
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "@/config/inngest";
+
+// Serve your Inngest functions as API endpoints
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
